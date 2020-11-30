7822828573bf6b9bc9282be22b4fc927276f935d
Katie Lee <g.hyunlee12@gmail.com>
2020-11-30 14:19:00 -0500

    Updated classes

--- kt_1.0/player/BlackPlayer.java
+++ kt_1.0/player/BlackPlayer.java
@@ -7,15 +7,16 @@ import java.util.List;
 
 public class BlackPlayer extends Player {
 
-    private String alliance; // black
-    private String boardSide; // top/bot
-    private String type; // comp/human
+    private String boardSide;   // top/bot
+    private String type;        // comp/human
     private Board board;
+    private List<Moves> legalMoves;
     private List<Piece> pieces;
-    private String state; // check/checkmate/stalemate/castled
+    private boolean isInCheck;
+    private boolean castled;
 
-    public BlackPlayer(String alliance, String boardSide, String type, Board board) {
-        super (alliance, boardSide, type, board);
+    public BlackPlayer(String boardSide, String type, Board board, List<Moves> legalMoves) {
+        super (boardSide, type, board, legalMoves);
         // generate the player's list of active pieces
         if (boardSide.equals("top")) {
             generatePieces(1,0);
@@ -23,15 +24,16 @@ public class BlackPlayer extends Player {
         else {
             generatePieces(6,7);
         }
-        state = null;
+        this.isInCheck = false;
     }
 
     /** Generate the player's pieces based on if they are on the top or bottom side of the board. */
-    private void generatePieces(int pawnRow, int pieceRow) {
+    @Override
+    public void generatePieces(int pawnRow, int pieceRow) {
         // add all the pawns to the list
         for (int i = 0; i< board.cols; i++) {
             int coords[] = new int[]{pawnRow,i};
-            this.pieces.add(i, new Pawn(coords, alliance, board));
+            this.pieces.add(i, new Pawn(coords, "black", board));
         }
         // add the remaining pieces to the list
         int i = 8;
@@ -40,21 +42,21 @@ public class BlackPlayer extends Player {
             switch (j) {
                 case (0):
                 case (7):
-                    this.pieces.add(i, new Rook(coords, alliance, board));
+                    this.pieces.add(i, new Rook(coords, "black", board));
                     break;
                 case (1):
                 case (6):
-                    this.pieces.add(i, new Knight(coords, alliance, board));
+                    this.pieces.add(i, new Knight(coords, "black", board));
                     break;
                 case (2):
                 case (5):
-                    this.pieces.add(i, new Bishop(coords, alliance, board));
+                    this.pieces.add(i, new Bishop(coords, "black", board));
                     break;
                 case (3):
-                    this.pieces.add(i, new Queen(coords, alliance, board));
+                    this.pieces.add(i, new Queen(coords, "black", board));
                     break;
                 case(4):
-                    this.pieces.add(i, new King(coords, alliance, board));
+                    this.pieces.add(i, new King(coords, "black", board));
                     break;
                 default:
                     break;
@@ -89,18 +91,22 @@ public class BlackPlayer extends Player {
     }
 
     /** Check if the player's move is a legal move. */
-    public boolean isMoveLegal(Piece piece, Tile destinationTile) {
-        // if player is in Check they must get out of check
-        if (this.state.equals("check")) {
-/////////////////AH. so much work.
-        }
-        if (piece.legalMoves().contains(destinationTile.getCoords())) {
-            return true;
+    public boolean isMoveLegal(Move move) {
+        return this.legalMoves.contains(move);
+    }
+
+    /** Check if player has any escape moves when in check. */
+    protected boolean hasEscapeMoves() {
+        for (final Moves move: this.legalMoves) {
+            final MoveTransition transition = makeMove(move);
+            if (transition.getMoveStatus().isDone()) {
+                return true;
+            }
         }
         return false;
     }
 
-    /** The player makes their move. */
+    /** The player makes their move.
     public Board makeMove(Piece piece, Tile destinationTile, Board board) {
         Board newBoard = new Board(board.getP1(), board.getP2());
         // if the opponent is occupying the destination tile
@@ -108,9 +114,9 @@ public class BlackPlayer extends Player {
             AttackMove attack = new AttackMove();
             newBoard = attack.move(piece, destinationTile, board);
 
-            /**
-             * ##this should belong to the attack move method:
-             * int[] initialCoords = piece.getPosition();
+
+             ##this should belong to the attack move method:
+             int[] initialCoords = piece.getPosition();
             int[] destinationCoords = tile.getPiece().getPosition();
             // move player's piece to their destination tile
             newBoard.getTile(destinationCoords[0],destinationCoords[1]).setPiece(piece);
@@ -120,7 +126,7 @@ public class BlackPlayer extends Player {
             newBoard.addDeadPiece(deadPiece);
             deadPiece.setState("dead");
             // remove piece from its initial position
-            newBoard.setTile(new EmptyTile(initialCoords),initialCoords);*/
+            newBoard.setTile(new EmptyTile(initialCoords),initialCoords);
 
         } // if the piece is moving to an empty tile
         else {
@@ -128,63 +134,25 @@ public class BlackPlayer extends Player {
            newBoard = aMove.move(piece, destinationTile, board);
         }
         return newBoard;
-    }
+    } */
 
-    /**
-     * Check if the player is in check state.
-     * @return true if the opponent is attacking the player's King piece with any of their pieces.
-     */
+    /** Check if the player is in check state. */
     public boolean isInCheck(Board board) {
-        if (!this.state.equals("checkMate") && !this.state.equals("staleMate")) {
-            // get the opponent player
-            Player player = (board.getP1().getAlliance().equals("white")) ? board.getP1() : board.getP2();
-            // check if any opponent's pieces can attack the player's King
-            for (Piece p : player.getActivePieces()) {
-                if (p.legalMoves().equals(this.getPiece(12).getPosition())) {
-                    this.state = "check";
-                    return true;
-                }
-            }
-        }
-        return false;
+        return this.isInCheck;
     }
 
-    /**
-     * Check if the player is in checkmate.
-     * @return true if the player's King piece is in check and cannot make any legal moves to break out of its state.
-     */
+    /** Check if the player is in checkmate. */
     public boolean isInCheckMate(Board board) {
-        // player must first be in Check
-        if (this.state.equals("check")) {
-            // get the opponent player
-            Player player = (board.getP1().getAlliance().equals("white")) ? board.getP1() : board.getP2();
-            // get the list of the player's King's legal moves
-            List<int[]> kingsMoves = this.getPiece(12).legalMoves();
-            // check for all opponent's pieces that can attack the player's King
-            for (Piece p : player.getActivePieces()) {
-                //if a piece's legal move is one of the king's legal moves - remove legal move from king
-                kingsMoves.removeIf(moves -> p.legalMoves().contains(moves));
-                if (kingsMoves.size()==0) {
-                    this.state = "checkmate";
-                    ////////////////this is HARD gotta check every piece - not just king - a piece can block for the king
-                }
-                if (p.legalMoves().) {
-                    this.state = "check";
-                    return true;
-                }
-            }
-        }
-        return false;
+        return this.isInCheck && !hasEscapeMoves();
     }
 
     /** Check if the player is in stalemate. */
     public boolean isInStaleMate() {
-        return false;
+        return !this.isInCheck && !hasEscapeMoves();
     }
 
     /** Check if the player is castled. */
     public boolean isCastled() {
-        return false;
+        return this.castled;
     }
-
 }
--- kt_1.0/player/Player.java
+++ kt_1.0/player/Player.java
@@ -7,15 +7,15 @@ import java.util.List;
 
 public abstract class Player {
 
-    private String alliance; // white/black
-    private String boardSide; // top/bot
-    private String type; // comp/human
+    private String boardSide;   // top/bot
+    private String type;        // comp/human
     private Board board;
-    private List<Piece> activePieces;
-    private String state; // check/checkmate/stalemate/castled
+    private List<Moves> legalMoves;
+    private List<Piece> pieces;
+    private boolean isInCheck;
+    private boolean castled;
 
-    public Player(String alliance, String boardSide, String type, Board board) {
-        this.alliance = alliance;
+    public Player(String boardSide, String type, Board board, List<Moves> legalMoves) {
         this.boardSide = boardSide;
         this.type = type;
         this.board = board;
@@ -29,41 +29,94 @@ public abstract class Player {
     }
 
     /** Generate the player's pieces based on if they are on the top or bottom side of the board. */
-    private void generatePieces(int pawnRow, int pieceRow) {
-        // add all the pawns to the list
-        for (int i = 0; i< board.cols; i++) {
-            int coords[] = new int[]{pawnRow,i};
-            this.activePieces.add(i, new Pawn(coords, alliance, board));
+    abstract void generatePieces(int pawnRow, int pieceRow);
+
+    /** Get the alliance of the player (white/black) */
+    public String getAlliance() {
+        return null;
+    }
+
+    /** Get the side of the board of the player (top/bot). */
+    public String getBoardSide() {
+        return null;
+    }
+
+    /** Get the type of the player (comp/human). */
+    public String getType() {
+        return null;
+    }
+
+    /** Get the player's list of active pieces. */
+    public List<Piece> getActivePieces() {
+        return null;
+    }
+
+    /** Get the player's active pieces at a given position. */
+    public Piece getActivePiece(int index) {
+        return null;
+    }
+
+    /** Check if the player's move is a legal move. */
+    public boolean isMoveLegal() {
+        return false;
+    }
+
+    /** The player makes their move. */
+    public void makeMove() {}
+
+    /** Check if the player is in check. */
+    public boolean isInCheck() {
+        return false;
+    }
+
+    /** Check if the player is in checkmate. */
+    public boolean isInCheckMate() {
+        return false;
+    }
+
+    /** Check if the player is in stalemate. */
+    public boolean isInStaleMate() {
+        return false;
+    }
+
+    /** Check if the player is castled. */
+    public boolean isCastled() {
+        return false;
+    }
+
+}
+package player;
+
+import board.*;
+import pieces.*;
+
+import java.util.List;
+
+public abstract class Player {
+
+    private String boardSide;   // top/bot
+    private String type;        // comp/human
+    private Board board;
+    private List<Moves> legalMoves;
+    private List<Piece> pieces;
+    private boolean isInCheck;
+    private boolean castled;
+
+    public Player(String boardSide, String type, Board board, List<Moves> legalMoves) {
+        this.boardSide = boardSide;
+        this.type = type;
+        this.board = board;
+        // generate the player's list of active pieces
+        if (boardSide.equals("top")) {
+            generatePieces(1,0);
         }
-        // add the remaining pieces to the list
-        int i = 8;
-        for (int j = 0; j < board.cols; j++) {
-            int coords[] = new int[]{pieceRow, j};
-            switch (j) {
-                case (0):
-                case (7):
-                    this.activePieces.add(i, new Rook(coords, alliance, board));
-                    break;
-                case (1):
-                case (6):
-                    this.activePieces.add(i, new Knight(coords, alliance, board));
-                    break;
-                case (2):
-                case (5):
-                    this.activePieces.add(i, new Bishop(coords, alliance, board));
-                    break;
-                case (3):
-                    this.activePieces.add(i, new Queen(coords, alliance, board));
-                    break;
-                case(4):
-                    this.activePieces.add(i, new King(coords, alliance, board));
-                    break;
-                default:
-                    break;
-            }
-            i++;
+        else {
+            generatePieces(6,7);
         }
     }
+
+    /** Generate the player's pieces based on if they are on the top or bottom side of the board. */
+    abstract void generatePieces(int pawnRow, int pieceRow);
 
     /** Get the alliance of the player (white/black) */
     public String getAlliance() {
