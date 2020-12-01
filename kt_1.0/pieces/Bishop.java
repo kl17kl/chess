ca73462a2addadb7eb4ba9ffd95afa4bc852cbc1
Katie Lee <g.hyunlee12@gmail.com>
2020-12-01 16:18:52 -0500

    Update Bishop.java

--- kt_1.0/pieces/Bishop.java
+++ kt_1.0/pieces/Bishop.java
@@ -9,9 +9,11 @@ public class Bishop extends Piece {
     private String alliance; // white or black
     private Board board;
     private List<int[]> legalMoves;
+    private String name;
 
     public Bishop(int[] position, String alliance, Board board) {
         super(position, alliance, board);
+        this.name = "Bishop";
     }
 
     /**
@@ -53,7 +55,10 @@ public class Bishop extends Piece {
                 if (!this.board.getTile(row,col).getPiece().getAlliance().equals(this.alliance)) {
                     meetsOpposite = true;
                 }
-                this.legalMoves.add(this.board.getTile(row,col).getCoords());
+                // cannot attack the opponent's King piece.
+                if (!this.board.getTile(row,col).getPiece().getName().equals("King")) {
+                    this.legalMoves.add(this.board.getTile(row,col).getCoords());
+                }
 
                 // look at the next diagonal tile
                 if (0 <= row+rowIncr && row+rowIncr <= board.rows && 0 <= col+colIncr && col+colIncr <= board.cols) {
